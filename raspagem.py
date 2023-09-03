8d8a7997bc67b83c0d1d0cc977189da424678f55
Jefferson Schittini <jefferschi@gmail.com>
2023-09-03 15:57:19 -0300

    com bs4 - inicio

--- raspagem.py
+++ raspagem.py
@@ -1,39 +1,51 @@
 import requests
+from time import sleep
+
 from bs4 import BeautifulSoup as bs
 import pandas as pd
+
+
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 from selenium import webdriver # implementar o driver do navegador
 
-# usando o webdriver-manager para não precisar instalar o driver específico do navegador manualmente.
+# configuraçõs do navegador. usando o webdriver-manager para não precisar instalar o driver específico do navegador manualmente.
 from webdriver_manager.chrome import ChromeDriverManager # driver do navegador. pode alterar o navegador
 from selenium.webdriver.chrome.service import Service # executa o webdriver manager
+from selenium.webdriver.chrome.options import Options # servirá para configurar tamanho da janela do navegador
+
+# usa opções para definir tamanho da tela
+opcoes = Options()
+#opcoes.add_argument('headless') # faz com que o navegador fique oculto.
+opcoes.add_argument('window-size=1350,700')
 
 servico = Service(ChromeDriverManager().install()) # instala o driver no ambiente local
 
 # Criar uma instância do WebDriver
-navegador = webdriver.Chrome(service=servico) # roda o navegador
+navegador = webdriver.Chrome(service=servico, options=opcoes) # roda o navegador
 
-maps = 'https://www.google.com.br/maps/'
-g1 = 'https://g1.globo.com/'
+pagina = 'https://www.google.com.br/maps/'
 busca = 'restaurante, Cariacica - ES'
 
-navegador.get(maps)
+navegador.get(pagina) # abre a página desejada
 
-"""
-copiar o xpath substitui copiar um elemento da página html, como uma classe, uma div, uma tag qualquer.
-Para consegui-lo, ao inspecionar uma página, clicar com o botão inverso sobre a área desejada no código,
-clicar em copy, e em XPath. 
+sleep(3) # tempo para que a página renderize e não corra o risco de o texto ser "digitado" antes
 
-"""
 # encontra o elemento xpath do campo desejado. No lugar de xpath pode colocar outro atributo como class e id
 elem_busca = navegador.find_element('xpath','//*[@id="searchboxinput"]')
 elem_busca.click() # seleciona com um click o elemento buscado
 elem_busca.send_keys(busca) # envia o texto digitado na variável ou por extenso dentro de aspas para o campo selecionado
 elem_busca.send_keys(Keys.RETURN) # tecla Enter após digitar a busca
 
-input('manter página aberta até uma informação do teclado')
+
+
+page_busca = bs(navegador.page_source,'html.parser')
+
+print(page_busca.prettify())
+
+
+input('pressione alguma tecla para interromper') # precisei colocar pois a página não permanecia aberta após rodar o script
 
 """
 proximos passos:
